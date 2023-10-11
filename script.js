1d22fde85fb47dc83725d42bf8d7f108828370fe
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-10-11 10:29:27 -0700

    suppertime

---
+++ script.js
@@ -0,0 +1,23 @@
+const hiTxt = document.getElementById("beep");
+
+hiTxt.addEventListener("click", chngTxt);
+
+function chngTxt(){
+    hiTxt.innerHTML = "hi";
+}
+// var number = 5;
+
+// if (number > 5){
+//     console.log("HOT FUDGE ON A SUNDAE")
+// }if (number < 5) {
+//     console.log(":( I sad..")
+// } else {
+//    console.log("beep boop") 
+// }
+
+// function sayHi(){
+//     console.log("H");
+//     console.log("I");
+// }
+
+// sayHi();
